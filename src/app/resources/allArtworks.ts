c605a4b2311ef76cf5cd0f95a187ff4f7a1b8680
colorimgugr <colorimg@go.ugr.es>
2025-07-01 14:58:30 +0200

    Add matVer 4 versions

--- src/app/resources/allArtworks.ts
+++ src/app/resources/allArtworks.ts
@@ -3,13 +3,16 @@ import type {
   Artwork,
 } from "@/app/resources/types";
 
-import mdv from "@/app/resources/artworks/mdv";
 import madFolgRaf from "@/app/resources/artworks/madFolgRaf";
 import transRaf from "@/app/resources/artworks/transRaf";
 import benClx from "@/app/resources/artworks/benClx";
 import martPrzAya from "@/app/resources/artworks/martPrzAya";
 import escTab from "@/app/resources/artworks/escTab";
+import matVerOrg from "@/app/resources/artworks/matVerOrg";
+import matVerRep from "@/app/resources/artworks/matVerRep";
+import matVerRest from "@/app/resources/artworks/matVerRest";
+import matVerSket from "@/app/resources/artworks/matVerSket";
 
 export const artworks: Artwork[] = [
-  mdv, madFolgRaf, transRaf, benClx, martPrzAya, escTab
+  madFolgRaf, transRaf, benClx, martPrzAya, escTab, matVerOrg, matVerRep, matVerRest, matVerSket
 ];
